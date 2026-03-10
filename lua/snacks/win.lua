5c77285dc1f1f220b5d6febb400916ab9738e9b3
Alex Lebedev <alexey.broadcast@gmail.com>
2026-03-10 11:35:51 +0300

    Update win.lua

--- lua/snacks/win.lua
+++ lua/snacks/win.lua
@@ -730,6 +730,9 @@ function M:open_win()
   end
   local opts = self:win_opts()
   if position == "float" then
+    if opts.anchor ~= "NW" and opts.anchor ~= "NE" and opts.anchor ~= "SW"  and opts.anchor ~= "SE" then
+      opts.anchor = nil
+    end
     self.win = vim.api.nvim_open_win(self.buf, enter, opts)
   elseif position == "current" then
     self.win = vim.api.nvim_get_current_win()
